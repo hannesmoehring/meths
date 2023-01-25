ab320f17802d7cd42a48967222e7c318282ea53f
Hannes Möhring <acc@hannesmoehring.de>
2023-01-25 10:23:29 +0100

    table for cayley

---
+++ cayley/table.py
@@ -0,0 +1,22 @@
+import csv, math
+
+group = []
+maxVal = 42
+
+for n in range(maxVal):
+    if math.gcd(maxVal, n) != 1:
+        continue
+    group.append(n)
+
+dic = dict()
+for y in group:
+    for x in group:
+        if y not in dic:
+            dic[y] = {x: (x * y) % maxVal}
+        else:
+            dic[y][x] = (x * y) % maxVal
+
+with open("hello45.csv", "w") as file:
+    w = csv.DictWriter(file, group)
+    for g in group:
+        w.writerow(dic[g])
