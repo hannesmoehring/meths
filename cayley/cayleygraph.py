74204dd69e6d4c10c64609ea3e1433dd9cd2d297
Hannes Möhring <acc@hannesmoehring.de>
2023-01-26 12:05:05 +0100

    possibility to chose wether displayed graphically or with text

--- cayley/cayleygraph.py
+++ cayley/cayleygraph.py
@@ -3,32 +3,49 @@ import math
 import networkx as nx
 import matplotlib.pylab as pylab
 
-gen = input("generator, if only use one gen   ")
+gen = int(input("generator, if only use one gen   "))
 if input("use only one gen? y/n   " ) == "y":
     useGen = True
 else: useGen = False
-maxVal = input("natural number: maxVal:  ")
+maxVal = int(input("natural number: maxVal:  "))
 group = []
+chooseDisplay = input("for graph, input g, for text, input t  ").lower()
 
 for n in range(maxVal):
     if math.gcd(maxVal, n) != 1:
         continue
     group.append(n)
 
-G = nx.Graph()
-for num in group:
-    G.add_node(num, size='small', name=num)
-
-if not useGen:
+if chooseDisplay == "g":
+    G = nx.Graph()
     for num in group:
-        if (num != 1):
-            for number in group:
-                G.add_edge(number, (number * num % maxVal))
+        G.add_node(num, size='small', name=num)
 
-if useGen:
+    if not useGen:
+        for num in group:
+            if (num != 1):
+                for number in group:
+                    G.add_edge(number, (number * num % maxVal))
+
+    if useGen:
+        for number in group:
+            G.add_edge(number, (number * gen % maxVal))
+
+    labels = nx.get_node_attributes(G, 'name')
+    nx.draw(G, labels=labels, node_size=500, pos=nx.circular_layout(G))
+    pylab.show()
+
+else:
+    print("generator : " + str(gen))
+    print("max : " + str(maxVal))
     for number in group:
-        G.add_edge(number, (number * gen % maxVal))
+        print("orgin: " + str(number) + " -> " + str(number * gen % maxVal))
 
-labels = nx.get_node_attributes(G, 'name')
-nx.draw(G, labels=labels, node_size=500, pos=nx.circular_layout(G))
-pylab.show()
+    if (input("Do you want full graph? y/n      ") == "y"):
+        for num in group:
+            print()
+            print("Generator now is : " + str(num))
+            print()
+            if (num != 1):
+                for number in group:
+                    print("orgin: " + str(number) + " -> " + str(number * num % maxVal))
